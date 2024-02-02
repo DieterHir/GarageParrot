9e3a59cb037692e9179fb6ae6cd80ba2f2d6c575
Dieter <dieter59136@live.fr>
2024-02-02 08:13:15 +0100

    ajout d'un header et d'un footer

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,6 +2,12 @@ import Route from "./Route.js";
 
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
+    new Route("/connexion", "Connexion", "/pages/connexion.html"),
+    new Route("/services", "Services", "./pages/services.html"),
+    new Route("/occasions", "Occasions", "/pages/occasions.html"),
+    new Route("/contact", "Contact", "/pages/contact.html"),
+    new Route("/avis", "Avis", "/pages/avis.html"),
+    new Route("/mod", "Modération", "/pages/mod.html"),
 ];
 
 export const websiteName = "Garage Parrot";
